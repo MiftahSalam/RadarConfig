7e00087e6c4f56ef2c993445e7080e9519c9491c
Miftah Salam <salam.miftah@gmail.com>
2022-03-26 07:41:21 +0700

    First implementation

---
+++ RadarConfig/radarconfig.cpp
@@ -0,0 +1,196 @@
+#include "radarconfig.h"
+
+#include <QFile>
+#include <QDir>
+#include <QSettings>
+
+RadarConfig::RadarConfig* RadarConfig::RadarConfig::instance{nullptr};
+QStringList RadarConfig::RadarConfig::nonVolatileKeys =
+        QStringList()<<NON_VOLATILE_PPI_DISPLAY_SHOW_RING
+                    <<NON_VOLATILE_PPI_DISPLAY_SHOW_COMPASS
+                   <<NON_VOLATILE_PPI_DISPLAY_SHOW_HEADING_MARKER
+                  <<NON_VOLATILE_PPI_DISPLAY_HEADING_UP
+                  <<NON_VOLATILE_PPI_DISPLAY_LAST_SCALE
+                 <<NON_VOLATILE_PPI_DISPLAY_SHOW_GZ
+                <<NON_VOLATILE_PPI_DISPLAY_SHOW_ARPA
+               <<NON_VOLATILE_RADAR_NET_IP_DATA
+              <<NON_VOLATILE_RADAR_NET_IP_REPORT
+             <<NON_VOLATILE_RADAR_NET_IP_CMD
+            <<NON_VOLATILE_RADAR_NET_PORT_DATA
+           <<NON_VOLATILE_RADAR_NET_PORT_REPORT
+          <<NON_VOLATILE_RADAR_NET_PORT_CMD
+         <<NON_VOLATILE_RADAR_TRAIL_ENABLE
+        <<NON_VOLATILE_RADAR_TRAIL_TIME
+       <<NON_VOLATILE_ARPA_PARAMS_MIN_CONTOUR_LEN
+      <<NON_VOLATILE_ARPA_PARAMS_SEARCH_RADIUS1
+     <<NON_VOLATILE_ARPA_PARAMS_SEARCH_RADIUS2
+    <<NON_VOLATILE_ARPA_PARAMS_MAX_TARGET_SIZE
+   <<NON_VOLATILE_ARPA_CONTROL_CREATE_ARPA_BY_CLICK
+  <<NON_VOLATILE_ARPA_NET_IP_SEND
+ <<NON_VOLATILE_ARPA_NET_PORT_SEND
+<<NON_VOLATILE_GZ_ENABLE_ALARM
+<<NON_VOLATILE_GZ_MODE
+<<NON_VOLATILE_GZ_TIMEOUT
+<<NON_VOLATILE_NAV_DATA_LAST_HEADING
+<<NON_VOLATILE_NAV_DATA_LAST_LATITUDE
+<<NON_VOLATILE_NAV_DATA_LAST_LONGITUDE
+<<NON_VOLATILE_NAV_CONTROL_GPS_AUTO
+<<NON_VOLATILE_NAV_CONTROL_HEADING_AUTO
+                      ;
+
+RadarConfig::RadarConfig::RadarConfig(QString path):filePath(path)
+{
+    QFile file(filePath);
+    if(filePath.isEmpty() || !file.exists())
+    {
+        filePath = QDir::homePath()+QDir::separator()+".radar.conf";
+        file.setFileName(filePath);
+    }
+
+    if(!file.exists()) initConfig();
+    else loadConfig();
+}
+
+bool RadarConfig::RadarConfig::setConfig(const QString &key, const QVariant &value)
+{
+    bool valid = false;
+    if(volatileVar.contains(key))
+    {
+        valid = true;
+        volatileVar.insert(key,value);
+    }
+    else if(nonVolatileVar.contains(key))
+    {
+        valid = true;
+        nonVolatileVar.insert(key,value);
+    }
+
+    return valid;
+}
+
+QVariant RadarConfig::RadarConfig::getConfig(const QString &key) const
+{
+    QVariant val;
+    if(volatileVar.contains(key)) val = volatileVar.value(key);
+    else if(nonVolatileVar.contains(key)) val = nonVolatileVar.value(key);
+
+    return val;
+}
+/*
+bool RadarConfig::RadarConfig::createVar(const QString &key, const QVariant &value, bool nonVolatile)
+{
+    bool valid = !volatileVar.contains(key) && !nonVolatileVar.contains(key);
+    if(valid)
+    {
+        if(nonVolatile) nonVolatileVar.insert(key,value);
+        else volatileVar.insert(key,value);
+    }
+
+    return valid;
+}
+*/
+void RadarConfig::RadarConfig::loadConfig()
+{
+    //volatile
+    volatileVar.insert(VOLATILE_GZ_CONFIRMED,true);
+
+    volatileVar.insert(VOLATILE_RADAR_PARAMS_FILTER_DATA_GAIN,0);
+    volatileVar.insert(VOLATILE_RADAR_PARAMS_FILTER_DATA_RAIN,0);
+    volatileVar.insert(VOLATILE_RADAR_PARAMS_FILTER_DATA_SEA,0);
+    volatileVar.insert(VOLATILE_RADAR_PARAMS_FILTER_DATA_MTI,0);
+    volatileVar.insert(VOLATILE_RADAR_PARAMS_FILTER_CONTROL_GAIN,true); //auto
+    volatileVar.insert(VOLATILE_RADAR_PARAMS_FILTER_CONTROL_RAIN,true); //auto
+    volatileVar.insert(VOLATILE_RADAR_PARAMS_FILTER_CONTROL_SEA,true); //auto
+    volatileVar.insert(VOLATILE_RADAR_PARAMS_FILTER_CONTROL_MTI,true); //enable
+    volatileVar.insert(VOLATILE_RADAR_PARAMS_FILTER_DATA_TARGET_BOOST,0);
+    volatileVar.insert(VOLATILE_RADAR_PARAMS_FILTER_DATA_INTERFERENCE,0);
+    volatileVar.insert(VOLATILE_RADAR_PARAMS_FILTER_DATA_TARGET_EXPAND,0);
+    volatileVar.insert(VOLATILE_RADAR_PARAMS_ALIGN_DATA_BEARING,0);
+    volatileVar.insert(VOLATILE_RADAR_PARAMS_ALIGN_DATA_ANTENA_HEIGHT,0);
+    volatileVar.insert(VOLATILE_RADAR_PARAMS_SCAN_DATA_SCAN_SPEED,0);
+    volatileVar.insert(VOLATILE_RADAR_PARAMS_SCAN_DATA_NOISE_REJECT,0);
+    volatileVar.insert(VOLATILE_RADAR_PARAMS_SCAN_DATA_TARGET_SEPARATION,0);
+    volatileVar.insert(VOLATILE_RADAR_PARAMS_SCAN_DATA_SIDE_LOBE_SUPPRESSION,0);
+    volatileVar.insert(VOLATILE_RADAR_PARAMS_SCAN_DATA_LOCAL_INTERFERENCE,0);
+    volatileVar.insert(VOLATILE_RADAR_PARAMS_RANGE_DATA_RANGE,0);
+
+    /*non volatile*/
+    QSettings config(filePath,QSettings::IniFormat);
+    foreach (QString key, config.allKeys()) nonVolatileVar.insert(key,config.value(key));
+
+    /*match desired keys*/
+    QMapIterator<QString, QVariant>i(nonVolatileVar);
+
+    while (i.hasNext())
+    {
+        i.next();
+        if(!nonVolatileKeys.contains(i.key())) qWarning()<<Q_FUNC_INFO<<"undesired key"<<i.key();
+    }
+}
+void RadarConfig::RadarConfig::initConfig()
+{
+    //non volatile
+    nonVolatileVar.insert(NON_VOLATILE_PPI_DISPLAY_SHOW_RING,false);
+    nonVolatileVar.insert(NON_VOLATILE_PPI_DISPLAY_HEADING_UP,false);
+    nonVolatileVar.insert(NON_VOLATILE_PPI_DISPLAY_SHOW_COMPASS,true);
+    nonVolatileVar.insert(NON_VOLATILE_PPI_DISPLAY_SHOW_HEADING_MARKER,true);
+    nonVolatileVar.insert(NON_VOLATILE_PPI_DISPLAY_LAST_SCALE,2000);
+    nonVolatileVar.insert(NON_VOLATILE_PPI_DISPLAY_SHOW_GZ,true);
+    nonVolatileVar.insert(NON_VOLATILE_PPI_DISPLAY_SHOW_ARPA,true);
+
+    nonVolatileVar.insert(NON_VOLATILE_RADAR_NET_IP_DATA,"127.0.0.1");
+    nonVolatileVar.insert(NON_VOLATILE_RADAR_NET_IP_REPORT,"127.0.0.1");
+    nonVolatileVar.insert(NON_VOLATILE_RADAR_NET_IP_CMD,"127.0.0.1");
+    nonVolatileVar.insert(NON_VOLATILE_RADAR_NET_PORT_DATA,6132);
+    nonVolatileVar.insert(NON_VOLATILE_RADAR_NET_PORT_REPORT,6133);
+    nonVolatileVar.insert(NON_VOLATILE_RADAR_NET_PORT_CMD,6131);
+    nonVolatileVar.insert(NON_VOLATILE_RADAR_TRAIL_ENABLE,true);
+    nonVolatileVar.insert(NON_VOLATILE_RADAR_TRAIL_TIME,15); //second
+
+    nonVolatileVar.insert(NON_VOLATILE_ARPA_PARAMS_MIN_CONTOUR_LEN,90);
+    nonVolatileVar.insert(NON_VOLATILE_ARPA_PARAMS_SEARCH_RADIUS1,10);
+    nonVolatileVar.insert(NON_VOLATILE_ARPA_PARAMS_SEARCH_RADIUS2,50);
+    nonVolatileVar.insert(NON_VOLATILE_ARPA_PARAMS_MAX_TARGET_SIZE,50);
+    nonVolatileVar.insert(NON_VOLATILE_ARPA_CONTROL_CREATE_ARPA_BY_CLICK,true);
+    nonVolatileVar.insert(NON_VOLATILE_ARPA_NET_IP_SEND,"127.0.0.1");
+    nonVolatileVar.insert(NON_VOLATILE_ARPA_NET_PORT_SEND,7132);
+
+    nonVolatileVar.insert(NON_VOLATILE_GZ_ENABLE_ALARM,true);
+    nonVolatileVar.insert(NON_VOLATILE_GZ_MODE,0); //arc mode
+    nonVolatileVar.insert(NON_VOLATILE_GZ_NOTIF_THRESHOLD,10);
+    nonVolatileVar.insert(NON_VOLATILE_GZ_TIMEOUT,90);
+
+    nonVolatileVar.insert(NON_VOLATILE_NAV_DATA_LAST_HEADING,0.0);
+    nonVolatileVar.insert(NON_VOLATILE_NAV_DATA_LAST_LATITUDE,0.0);
+    nonVolatileVar.insert(NON_VOLATILE_NAV_DATA_LAST_LONGITUDE,0.0);
+    nonVolatileVar.insert(NON_VOLATILE_NAV_CONTROL_GPS_AUTO,true);
+    nonVolatileVar.insert(NON_VOLATILE_NAV_CONTROL_HEADING_AUTO,true);
+
+}
+
+void RadarConfig::RadarConfig::saveConfig()
+{
+    QSettings config(filePath,QSettings::IniFormat);
+    QMapIterator<QString, QVariant>i(nonVolatileVar);
+
+    while (i.hasNext())
+    {
+        i.next();
+        config.setValue(i.key(),i.value());
+    }
+}
+
+RadarConfig::RadarConfig::~RadarConfig()
+{
+    saveConfig();
+}
+RadarConfig::RadarConfig* RadarConfig::RadarConfig::getInstance(const QString& path )
+{
+    if(instance == nullptr) instance = new RadarConfig(path);
+    else
+    {
+        if(instance->filePath != path && !path.isEmpty()) return nullptr;
+    }
+
+    return  instance;
+}
---
+++ RadarConfig/radarconfig.h
@@ -0,0 +1,103 @@
+#ifndef RADARCONFIG_H
+#define RADARCONFIG_H
+
+#include "radarconfig_global.h"
+#include <QVariant>
+
+namespace RadarConfig {
+
+const QString NON_VOLATILE_PPI_DISPLAY_SHOW_RING = "PPI/Display/show_ring";
+const QString NON_VOLATILE_PPI_DISPLAY_SHOW_COMPASS = "PPI/Display/heading_up";
+const QString NON_VOLATILE_PPI_DISPLAY_SHOW_HEADING_MARKER = "PPI/Display/show_heading_marker";
+const QString NON_VOLATILE_PPI_DISPLAY_HEADING_UP = "PPI/Display/heading_up";
+const QString NON_VOLATILE_PPI_DISPLAY_LAST_SCALE = "PPI/Display/last_scale";
+const QString NON_VOLATILE_PPI_DISPLAY_SHOW_GZ = "PPI/Display/show_gz";
+const QString NON_VOLATILE_PPI_DISPLAY_SHOW_ARPA = "PPI/Display/show_arpa";
+
+const QString NON_VOLATILE_RADAR_NET_IP_DATA = "Radar/net/ip_data";
+const QString NON_VOLATILE_RADAR_NET_IP_REPORT = "Radar/net/ip_report";
+const QString NON_VOLATILE_RADAR_NET_IP_CMD = "Radar/net/ip_command";
+const QString NON_VOLATILE_RADAR_NET_PORT_DATA = "Radar/net/port_data";
+const QString NON_VOLATILE_RADAR_NET_PORT_REPORT = "Radar/net/port_report";
+const QString NON_VOLATILE_RADAR_NET_PORT_CMD = "Radar/net/port_command";
+const QString NON_VOLATILE_RADAR_TRAIL_ENABLE = "Radar/trail/enable";
+const QString NON_VOLATILE_RADAR_TRAIL_TIME = "Radar/trail/time";
+
+const QString NON_VOLATILE_ARPA_PARAMS_MIN_CONTOUR_LEN = "Arpa/params/min_contour_len";
+const QString NON_VOLATILE_ARPA_PARAMS_SEARCH_RADIUS1 = "Arpa/params/search_radius1";
+const QString NON_VOLATILE_ARPA_PARAMS_SEARCH_RADIUS2 = "Arpa/params/search_radius2";
+const QString NON_VOLATILE_ARPA_PARAMS_MAX_TARGET_SIZE = "Arpa/params/max_target_size";
+const QString NON_VOLATILE_ARPA_CONTROL_CREATE_ARPA_BY_CLICK = "Arpa/control/create_arpa_by_click";
+const QString NON_VOLATILE_ARPA_NET_IP_SEND = "Arpa/net/ip_send";
+const QString NON_VOLATILE_ARPA_NET_PORT_SEND = "Arpa/net/port_send";
+
+const QString NON_VOLATILE_GZ_ENABLE_ALARM = "GZ/enable_alarm";
+const QString NON_VOLATILE_GZ_MODE = "GZ/mode";
+const QString NON_VOLATILE_GZ_NOTIF_THRESHOLD = "GZ/notif_threshold";
+const QString NON_VOLATILE_GZ_TIMEOUT = "GZ/timeout";
+
+const QString NON_VOLATILE_NAV_DATA_LAST_HEADING = "Nav/data/last_heading";
+const QString NON_VOLATILE_NAV_DATA_LAST_LATITUDE = "Nav/data/last_lat";
+const QString NON_VOLATILE_NAV_DATA_LAST_LONGITUDE = "Nav/data/last_lon";
+const QString NON_VOLATILE_NAV_CONTROL_GPS_AUTO = "Nav/control/gps_auto";
+const QString NON_VOLATILE_NAV_CONTROL_HEADING_AUTO = "Nav/control/heading_auto";
+
+const QString VOLATILE_GZ_CONFIRMED = "GZ/confirmed";
+
+const QString VOLATILE_RADAR_PARAMS_FILTER_DATA_GAIN = "Radar/params/filter/data/gain";
+const QString VOLATILE_RADAR_PARAMS_FILTER_DATA_RAIN = "Radar/params/filter/data/rain";
+const QString VOLATILE_RADAR_PARAMS_FILTER_DATA_SEA = "Radar/params/filter/data/sea";
+const QString VOLATILE_RADAR_PARAMS_FILTER_DATA_MTI = "Radar/params/filter/data/mti";
+const QString VOLATILE_RADAR_PARAMS_FILTER_CONTROL_GAIN = "Radar/params/filter/control/gain";
+const QString VOLATILE_RADAR_PARAMS_FILTER_CONTROL_RAIN = "Radar/params/filter/control/rain";
+const QString VOLATILE_RADAR_PARAMS_FILTER_CONTROL_SEA = "Radar/params/filter/control/sea";
+const QString VOLATILE_RADAR_PARAMS_FILTER_CONTROL_MTI = "Radar/params/filter/control/mti";
+const QString VOLATILE_RADAR_PARAMS_FILTER_DATA_TARGET_BOOST = "Radar/params/filter/data/targetBoost";
+const QString VOLATILE_RADAR_PARAMS_FILTER_DATA_INTERFERENCE = "Radar/params/filter/data/LInterference";
+const QString VOLATILE_RADAR_PARAMS_FILTER_DATA_TARGET_EXPAND = "Radar/params/filter/data/targetExpan";
+const QString VOLATILE_RADAR_PARAMS_ALIGN_DATA_BEARING = "Radar/params/align/data/bearing";
+const QString VOLATILE_RADAR_PARAMS_ALIGN_DATA_ANTENA_HEIGHT = "Radar/params/align/data/antena_height";
+const QString VOLATILE_RADAR_PARAMS_SCAN_DATA_SCAN_SPEED = "Radar/params/scan/data/scan_speed";
+const QString VOLATILE_RADAR_PARAMS_SCAN_DATA_NOISE_REJECT = "Radar/params/scan/data/noise_reject";
+const QString VOLATILE_RADAR_PARAMS_SCAN_DATA_TARGET_SEPARATION = "Radar/params/scan/data/target_sep";
+const QString VOLATILE_RADAR_PARAMS_SCAN_DATA_SIDE_LOBE_SUPPRESSION = "Radar/params/scan/data/side_lobe_suppression";
+const QString VOLATILE_RADAR_PARAMS_SCAN_DATA_LOCAL_INTERFERENCE = "Radar/params/scan/data/local_interference_rejection";
+const QString VOLATILE_RADAR_PARAMS_RANGE_DATA_RANGE = "Radar/params/range/data/range";
+
+enum RadarState
+{
+    RADAR_OFF,
+    RADAR_STANDBY,
+    RADAR_TRANSMIT,
+    RADAR_WAKING_UP
+};
+
+class RADARCONFIGSHARED_EXPORT RadarConfig
+{
+public:
+    RadarConfig(RadarConfig& other) = delete;
+    void operator=(const RadarConfig&) = delete;
+
+    static RadarConfig* getInstance(const QString &path);
+//    bool createVar(const QString& key, const QVariant& value, bool nonVolatile);
+    bool setConfig(const QString& key, const QVariant& value);
+    QVariant getConfig(const QString& key) const;
+
+protected:
+    RadarConfig(QString path="");
+    ~RadarConfig();
+
+private:
+    void initConfig();
+    void loadConfig();
+    void saveConfig();
+
+    static RadarConfig* instance;
+    static QStringList nonVolatileKeys;
+
+    QString filePath;
+    QMap<QString,QVariant> nonVolatileVar, volatileVar;
+};
+}
+
+#endif // RADARCONFIG_H
